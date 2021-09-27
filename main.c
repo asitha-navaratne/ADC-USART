c528c1ee4754769c12fef595fa2693b352af12f4
Asitha Navaratne <asithan476@gmail.com>
2021-09-27 22:13:30 +0530

    Justified comments

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ void DECODE_VALUE(uint16_t value);
 void USART_SEND(unsigned char character);
 void USART_SEND_ARRAY(uint8_t *arrayPointer);
 
-uint8_t UBRR = 51;								///< Value to be changed to specify the baudrate for the particular operating clock frequency.
+uint8_t UBRR = 51;							///< Value to be changed to specify the baudrate for the particular operating clock frequency.
 
 unsigned char transmit[4];
 
@@ -33,7 +33,7 @@ int main(void){
 
 void ADC_INIT(void){
 	ADMUX = ((1<<REFS0)|(1<<MUX4));					///< Set ADC reference to AVCC and analog input mode to differential input of ADC0 and ADC1.
-	ADCSRA = ((1<<ADEN)|(1<<ADPS2)|(1<<ADPS1));		///< Enable ADC and set ADC prescalar to 64.
+	ADCSRA = ((1<<ADEN)|(1<<ADPS2)|(1<<ADPS1));			///< Enable ADC and set ADC prescalar to 64.
 }
 
 /*!
@@ -41,9 +41,9 @@ void ADC_INIT(void){
  */
 
 void USART_INIT(void){
-	UCSRB |= (1<<TXEN);								///< Enable transmission over USART.
-	UCSRC |= (1<<URSEL)|(1<<UCSZ1)|(1<<UCSZ0);		///< Select register UCSRC and set transmission character size to 8 bits.
-	UBRRL = UBRR;										///< Set UBRR value for specified baudrate at specified frequency.
+	UCSRB |= (1<<TXEN);						///< Enable transmission over USART.
+	UCSRC |= (1<<URSEL)|(1<<UCSZ1)|(1<<UCSZ0);			///< Select register UCSRC and set transmission character size to 8 bits.
+	UBRRL = UBRR;							///< Set UBRR value for specified baudrate at specified frequency.
 }
 
 /*!
@@ -52,9 +52,9 @@ void USART_INIT(void){
  */
 
 uint16_t ADC_CONVERSION(void){
-	ADCSRA |= (1<<ADSC);								///< Start ADC conversion.
-	while(ADCSRA & (1<<ADSC));							///< Wait until conversion is complete.
-	return ADC;											///< Return result of conversion.
+	ADCSRA |= (1<<ADSC);						///< Start ADC conversion.
+	while(ADCSRA & (1<<ADSC));					///< Wait until conversion is complete.
+	return ADC;							///< Return result of conversion.
 }
 
 /*!
@@ -63,10 +63,10 @@ uint16_t ADC_CONVERSION(void){
  */
 
 void DECODE_VALUE(uint16_t value){
-	transmit[0] = 48 + (value/1000);					///< Decode first digit of the value.
+	transmit[0] = 48 + (value/1000);				///< Decode first digit of the value.
 	transmit[1] = 48 + ((value%1000)/100);				///< Decode second digit of the value.
 	transmit[2] = 48 + ((value%100)/10);				///< Decode third digit of the value.
-	transmit[3] = 48 + (value%10);						///< Decode last digit of the value.
+	transmit[3] = 48 + (value%10);					///< Decode last digit of the value.
 }
 
 /*!
@@ -75,9 +75,9 @@ void DECODE_VALUE(uint16_t value){
  */
 
 void USART_SEND(unsigned char character){
-	while(!(UCSRA & (1<<UDRE)));						///< Wait until data register is empty.
-	UDR = character;									///< Load character to be transmitted to data register.
-	while(!(UCSRA & (1<<TXC)));						///< Wait until transmission is complete.
+	while(!(UCSRA & (1<<UDRE)));					///< Wait until data register is empty.
+	UDR = character;						///< Load character to be transmitted to data register.
+	while(!(UCSRA & (1<<TXC)));					///< Wait until transmission is complete.
 }
 
 /*!
@@ -89,5 +89,5 @@ void USART_SEND_ARRAY(uint8_t *arrayPointer){
 	for(uint8_t i=0;i<4;i++){
 		USART_SEND(arrayPointer[i]);
 	}
-	USART_SEND(32);										///< Transmit a space character.
+	USART_SEND(32);							///< Transmit a space character.
 }
